f18c6738a26e6897b39d968f94fa492b6b24230d
Rhalf Wendel David Caacbay <rhalfcaacbay@gmail.com>
2015-11-15 17:21:35 +0300

    20151115
    -Added TabItems on TqatProSocketTool
    -Added ServerProfile with INotifyPropertyChanged.

--- TqatProSocketTool/MainWindow.xaml.cs
+++ TqatProSocketTool/MainWindow.xaml.cs
@@ -13,23 +13,123 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using TqatProSocketTool.ViewModel;
+using AtsGps;
+using AtsGps.Meitrack;
 
 namespace TqatProSocketTool {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// </summary> 
     public partial class MainWindow : Window {
         public MainWindow () {
             InitializeComponent();
         }
 
+        List<TcpManager> tcpManagers = new List<TcpManager>();
+        List<ServerProfile> serverProfiles = new List<ServerProfile>();
+
+
+
         private void Window_Loaded (object sender, RoutedEventArgs e) {
             this.Title = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + " - " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
 
-            ViewDashBoard viewDashBoard = new ViewDashBoard();
-            textBlockDashBoard.DataContext = viewDashBoard;
+            Machine machine = new Machine();
+
+            labelOperatingSystem.DataContext = machine;
+            labelName.DataContext = machine;
+            comboBoxIps.DataContext = machine;
+            labelLocalDateTime.DataContext = machine;
+            labelUtcDateTime.DataContext = machine;
+            labelCore.DataContext = machine;
 
 
+
+            ServerProfile spMeitrackMvt100 = new ServerProfile();
+            spMeitrackMvt100.IpAddresses = machine.IpAddresses;
+            spMeitrackMvt100.Brand = "Meitrack";
+            spMeitrackMvt100.Model = "Mvt100";
+            spMeitrackMvt100.Port = 8887;
+            spMeitrackMvt100.IsEnabled = true;
+            serverProfiles.Add(spMeitrackMvt100);
+
+            ServerProfile spMeitrackT1 = new ServerProfile();
+            spMeitrackT1.IpAddresses = machine.IpAddresses;
+            spMeitrackT1.Brand = "Meitrack";
+            spMeitrackT1.Model = "T1";
+            spMeitrackT1.Port = 4000;
+            spMeitrackT1.IsEnabled = true;
+            serverProfiles.Add(spMeitrackT1);
+
+
+
+            foreach (ServerProfile sp in serverProfiles) {
+                listViewServerProfile.Items.Add(sp);
+            }
+        }
+
+        private void Button_Click (object sender, RoutedEventArgs e) {
+            try {
+                Button button = (Button)sender;
+                if ((String)button.Content == "Start") {
+                   
+                    foreach (ServerProfile sp in serverProfiles) {
+                        //Mvt100
+                        if (sp.IsEnabled == true && sp.Brand == "Meitrack" && sp.Model == "Mvt100") {
+                            listViewServerProfileStatus.Items.Add(sp);
+                            MeitractTcpManager meitractTcpManagerMvt100 = new MeitractTcpManager(sp.IpAddress, sp.Port);
+                            meitractTcpManagerMvt100.Event += MeitractTcpManagerMvt100_Event;
+                            meitractTcpManagerMvt100.DataReceived += MeitractTcpManagerMvt100_DataReceived;
+                            meitractTcpManagerMvt100.Start();
+                            tcpManagers.Add(meitractTcpManagerMvt100);
+                        }
+                        //T1
+                        if (sp.IsEnabled == true && sp.Brand == "Meitrack" && sp.Model == "T1") {
+                            listViewServerProfileStatus.Items.Add(sp);
+                            MeitractTcpManager meitractTcpManagerT1 = new MeitractTcpManager(sp.IpAddress, sp.Port);
+                            meitractTcpManagerT1.Event += MeitractTcpManagerT1_Event;
+                            meitractTcpManagerT1.DataReceived += MeitractTcpManagerT1_DataReceived;
+                            meitractTcpManagerT1.Start();
+                            tcpManagers.Add(meitractTcpManagerT1);
+                        }
+
+                    }
+
+                    button.Content = "Stop";
+                    groupServerProfiles.IsEnabled = false;
+                } else {
+                    button.Content = "Start";
+                    groupServerProfiles.IsEnabled = true;
+                    listViewServerProfileStatus.Items.Clear();
+                    foreach (TcpManager tcpManager in tcpManagers) {
+                        tcpManager.Stop();
+                    }
+                    tcpManagers.Clear();
+                }
+            } catch (Exception exception) {
+                MessageBox.Show(exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void MeitractTcpManagerT1_DataReceived (byte[] data) {
+
+        }
+
+        private void MeitractTcpManagerT1_Event (ServerLog serverLog) {
+            serverLog.Description = "T1 - " + serverLog.Description;
+            Dispatcher.BeginInvoke(new Action(() => {
+                dataGridLog.Items.Add(serverLog);
+            }));
+        }
+
+        private void MeitractTcpManagerMvt100_DataReceived (byte[] data) {
+      
+        }
+
+        private void MeitractTcpManagerMvt100_Event (ServerLog serverLog) {
+            serverLog.Description = "Mvt100 - " + serverLog.Description;
+            Dispatcher.BeginInvoke(new Action(() => {
+                dataGridLog.Items.Add(serverLog);
+            }));
         }
     }
 }
--- TqatProSocketTool/ViewModel/Machine.cs
+++ TqatProSocketTool/ViewModel/Machine.cs
@@ -1,28 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace TqatProSocketTool.ViewModel {
-    public class ViewDashBoard {
-        public ViewDashBoard () {
+    public class Machine : INotifyPropertyChanged {
 
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged (String propertyName) {
+            if (PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
-
-        public string MachineOperatingSystem {
+        public Machine() {
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(1000);
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Start();
+        }
+        private void dispatcherTimer_Tick (object sender, EventArgs e) {
+            this.LocalDateTime = System.DateTime.Now.ToLocalTime().ToString("yyyy/MM/dd HH:mm:ss");
+            this.UtcDateTime = System.DateTime.Now.ToUniversalTime().ToString("yyyy/MM/dd HH:mm:ss");
+            OnPropertyChanged("UtcDateTime");
+            OnPropertyChanged("LocalDateTime");
+        }
+        public String OperatingSystem {
             get {
+                //string bit = "";
+                //if (System.Environment.Is64BitOperatingSystem) {
+                //    bit = " 64 bit";
+                //} else {
+                //    bit = " 32 bit";
+                //}
                 return System.Environment.OSVersion.ToString();
             }
         }
+        public String Name {
+            get {
+                return System.Environment.MachineName;
+            }
+        }
+        public List<String> IpAddresses {
+            get {
+                IPHostEntry host;
+                host = Dns.GetHostEntry(Dns.GetHostName());
 
-        public string MachineName {
+
+                List<String> listIp = new List<String>();
+                foreach (IPAddress ip in host.AddressList) {
+                    if (ip.AddressFamily.ToString() == "InterNetwork") {
+                        if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork) {
+                            listIp.Add(ip.ToString());
+                        }
+                    }
+                }
+                return listIp;
+            }
+        }
+        public string LocalDateTime {
             get;
             set;
         }
-        public string Ip {
+        public string UtcDateTime {
             get;
             set;
         }
+        public string Processor {
+            get {
+                return System.Environment.ProcessorCount.ToString();
+            }
+        }
+
     }
 }
